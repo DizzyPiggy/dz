330882b46879e572eb409e185786e31f20be4226
Timur <raggimovtimur@gmail.com>
2025-10-18 11:12:17 +0300

    Initial Commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5, };
+            Console.WriteLine("Введите индекс:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                Console.WriteLine(array[a]);
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Ошибка: индекс вне диапазона!");
+            }
+        }
+    }
+}
